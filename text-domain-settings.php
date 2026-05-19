1212f5d3f96dbc52d0b5ba6036ebb0592739e363
Tawhidur Rahman Dear <114006190+tawhidurrahmandear@users.noreply.github.com>
2026-05-19 20:39:10 +0600

    Add files via upload

---
+++ text-domain-settings.php
@@ -0,0 +1,55 @@
+<?php 
+
+// Prevent direct file access
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+?>
+
+<div class="wrap">
+<h2 style="text-align: center;"><span style="font-family: 'Ubuntu', sans-serif; color: #000000;">"Plugin Name"</span></h2>
+<br><br>
+</div>
+
+<p class="outgoing-links">
+
+<a href="https://example.com" rel="noopener"><button class="outgoing_button" type="button">Example website</button></a>
+
+<a href="https://example.com" rel="noopener" target="_blank"><button class="outgoing_button" type="button">Example website with new window</button></a>
+
+<a href="#" rel="noopener"><button class="outgoing_button" type="button">Dummy Links</button></a>
+  
+<a href="#" rel="noopener"><button class="outgoing_button" type="button">This button has a really long text that you can see what happens in mobile phone mode, also in desktop mode. </button></a>
+
+</p> 
+
+<style>
+  .outgoing-links {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center; 
+  }
+  
+.outgoing_button {
+    font-family: Tahoma, sans-serif;
+    font-size: 15px;
+    font-weight: bold;
+    color: #ffffff;
+    background-color: #6600FF;
+    padding: 15px;
+    margin: 5px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.5s ease;
+}
+
+.outgoing_button:hover {
+    background-color: #e0ccff;
+    color: #000000;
+    transition: background-color 0.5s ease;
+}
+</style>
+
+<p style="padding: 20px; text-align: justify;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc sed id semper risus in hendrerit gravida rutrum. Sem et tortor consequat id porta nibh venenatis cras. A diam sollicitudin tempor id eu nisl. Malesuada proin libero nunc consequat. Sollicitudin tempor id eu nisl nunc mi ipsum. Lacus sed viverra tellus in. Tellus cras adipiscing enim eu turpis egestas pretium aenean pharetra. Felis eget velit aliquet sagittis id. Neque sodales ut etiam sit. Blandit volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque. Orci porta non pulvinar neque laoreet suspendisse interdum consectetur. Et ultrices neque ornare aenean euismod elementum. Ultrices gravida dictum fusce ut placerat. Leo integer malesuada nunc vel risus commodo viverra maecenas accumsan. Rhoncus urna neque viverra justo. Dignissim enim sit amet venenatis urna cursus. Duis convallis convallis tellus id interdum velit laoreet id.</p>
